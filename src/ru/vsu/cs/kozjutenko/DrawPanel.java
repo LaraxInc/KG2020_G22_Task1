be2ba3b6c4287a125efcdfe12e83b29997964218
LaraxInc <71033016+LaraxInc@users.noreply.github.com>
2020-10-10 11:41:09 +0300

    Update DrawPanel.java

--- src/ru/vsu/cs/kozjutenko/DrawPanel.java
+++ src/ru/vsu/cs/kozjutenko/DrawPanel.java
@@ -14,8 +14,8 @@ public class DrawPanel extends JPanel {
 
         gr.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         drawBasis(gr,getWidth(), getHeight(), new Color(128, 0, 0), new Color(160, 82, 45));
-        drawSail(gr, new Color(255, 248, 220));
-        drawWindows(gr, 240, 430, 40, 40, Color.white);
+        drawSail(gr, getWidth(), getHeight(), new Color(255, 248, 220));
+        drawWindows(gr, (int)(0.85*getWidth()-420), (int)(0.255*getHeight()+295), 40, 40, new Color(255, 248, 220));
         drawSun(gr, 700, 80, 40, 90, 28, Color.yellow);
 
         gr.setColor(Color.BLACK);
@@ -25,28 +25,28 @@ public class DrawPanel extends JPanel {
     }
 
     public void drawBasis(Graphics2D gr, int width, int height, Color color, Color colorMast) {
-        int[] ArrayX = {(int)(0.25*width), (int) (0.75*width), (int) (0.85*width), (int) (0.15*width)};
-        int[] ArrayY = {(int)(0.85*height), (int)(0.85*height), (int)(0.7*height), (int)(0.7*height)};
+        int[] ArrayX = {(int)(0.85*width-470), (int) (0.85*width-80), (int) (0.85*width), (int) (0.85*width-550)};
+        int[] ArrayY = {(int)(0.255*height+340), (int)(0.255*height+340), (int)(0.255*height+250), (int)(0.255*height+250)};
         Polygon p = new Polygon(ArrayX, ArrayY, 4);
         gr.drawPolygon(p);
         gr.setColor(color);
         gr.fillPolygon(p);
         gr.setColor(colorMast);
-        gr.fillRect(390, 150, 10, 250);
+        gr.fillRect((int)(0.85*width-285), (int)(0.255*height), 10, 250);
     }
 
-    public static void drawSail(Graphics2D gr, Color color) {
-        int[] ArrayX1 = {400, 570, 400};
-        int[] ArrayY2 = {155, 350, 350};
+    public static void drawSail(Graphics2D gr, int width, int height, Color color) {
+        int[] ArrayX1 = {(int)(0.85*width-275), (int)(0.85*width-140), (int)(0.85*width-275)};
+        int[] ArrayY2 = {(int)(0.255*height), (int)(0.255*height+220), (int)(0.255*height+220)};
         Polygon pl = new Polygon(ArrayX1, ArrayY2, 3);
         gr.drawPolygon(pl);
         gr.setColor(color);
         gr.fillPolygon(pl);
     }
 
-    public static void drawWindows(Graphics2D gr, int x, int y, int width, int height, Color color) {
+    public static void drawWindows(Graphics2D gr, int x, int y,int width, int height, Color color) {
         gr.setColor(color);
-        for (int i = 240; i < 600; i += 140) {
+        for (int i = (int)(0.85*width-420); i < (int)(0.85*width); i += 140) {
             gr.fillOval(i, y, width, height);
         }
     }
@@ -64,4 +64,3 @@ public class DrawPanel extends JPanel {
         }
     }
 }
-
